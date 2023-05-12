1da5ac633c0ea4f8cb7dadb81780c3596225225b
RaKAsHASH <testwinhelp@gmail.com>
2023-05-12 15:40:08 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Form from './components/index';
+
+function App() {
+   return (
+      <div>
+         <Form />
+      </div>
+   );
+}
+
+export default App;
+
